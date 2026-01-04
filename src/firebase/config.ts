ecf0cf65f3b6074a6b31a61c5a9b76510dd5eba9
ANUSHA SINGH RAJPUT 24BAI10549 <anusha.24bai10549@vitbhopal.ac.in>
2026-01-04 19:15:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4871136705-8cc96",
+  "appId": "1:329296722048:web:d3ecacb88d87e4e393ce65",
+  "apiKey": "AIzaSyBbzStOTzhjnxzFyrx5YficUd9Zu3tshX4",
+  "authDomain": "studio-4871136705-8cc96.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "329296722048"
+};
